303a2fcb45e6c12cc6f7bdf018658718a5c58c5e
calam1 <chrislam67@gmail.com>
2014-06-11 14:52:07 -0500

    fetch effective deals

--- src/interop/core.clj
+++ src/interop/core.clj
@@ -6,12 +6,17 @@
   (:gen-class :name com.mongodb.dao.Deals
    :methods [#^{:static false} [saveDeal [java.lang.String] java.lang.String]
              #^{:static false} [getDealsByIds [java.lang.String java.util.Set] java.lang.String]
-             #^{:static false} [saveEffectiveDeals [java.util.List] void]]))
+             #^{:static false} [saveEffectiveDeals [java.util.List] void]
+             #^{:static false} [getEffectiveDeals [java.lang.String java.lang.String java.lang.String] java.lang.String]]))
 
 (def andQuery {"$and" "queries"})
 (def tenantQuery {:id "tenantId"})
 (def dealIdQuery {:deal-id {$in ["deal-ids"]}})
 (def excludedData {:_id 0})
+(def tenantIdQuery {:tenant-id "tenantId"})
+(def brandQuery {:brand-id "brandId"})
+(def regexQuery {"$regex" "regexQuery"})
+(def startDateTimeQuery {:start-datetime "startDateTime"})
 
 (defn- write-json-mongodb-objectid [x out]
   (json/write (str x) out))
@@ -45,13 +50,29 @@
 
 (defn saveEffectiveDeals
   [effectiveDeals]
-  (println "argument: " effectiveDeals)
   (let [conn (mongo/connect {:host "127.0.0.1" :port 27017})
         db (mongo/get-db conn "effectivedealsdb")]
     (let [jsonData  (map json/read-str effectiveDeals)
           jsonKeyWord (clojure.walk/keywordize-keys jsonData)]
-      (println "test1: " jsonKeyWord)
     (mc/insert-batch db "effectivedeals" jsonKeyWord))))
+
+(defn updateEffectiveDealsQuery
+  [tenantId brandId dateSearch]
+  (let [tenantUpdatedQuery (assoc-in tenantIdQuery [:tenant-id] tenantId)
+        brandUpdatedQuery (assoc-in brandQuery [:brand-id] brandId)
+        regexUpdatedQuery (assoc-in regexQuery ["$regex"] (str dateSearch ".*"))
+        startDateTimeUpdatedQuery (assoc-in startDateTimeQuery [:start-datetime] regexUpdatedQuery)
+        partialQuery (vector tenantUpdatedQuery brandUpdatedQuery startDateTimeUpdatedQuery)]
+    (assoc-in andQuery ["$and"] partialQuery)))
+
+(defn getEffectiveDeals
+  [tenantId brandId dateSearch]
+  (let [conn (mongo/connect {:host "127.0.0.1" :port 27017})
+        db (mongo/get-db conn "effectivedealsdb")
+        collection "effectivedeals"
+        query (updateEffectiveDealsQuery tenantId brandId dateSearch)
+        data (mc/find-maps db collection query excludedData)]
+    (json/write-str data)))
 
 (defn -saveDeal
   [this deal]
@@ -64,6 +85,14 @@
 (defn -saveEffectiveDeals
   [this effectiveDeals]
   (saveEffectiveDeals effectiveDeals))
+
+(defn -getEffectiveDeals
+  [this tenantId brandId dateSearch]
+  (getEffectiveDeals tenantId brandId dateSearch))
+
+(defn testQuery
+  []
+  (updateEffectiveDealsQuery "tenant" "brand" "date"))
 
 (defn -main
   [& args])
